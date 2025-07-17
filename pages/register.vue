08b4d52db17fdbfb6c50e68e674a93c8394a86c3
@agungwramdev <bzee71@gmail.com>
2025-07-17 09:14:30 +0700

    Project Setup

---
+++ pages/register.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex bg-black h-screen">
+        <!-- sidebar -->
+         <div class="bg-zinc-900 w-[516px]">
+            <Logo />
+         </div>
+         <!-- /sidebar -->
+    </div>
+</template>
+
